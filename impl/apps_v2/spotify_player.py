56b26e51737c706c322c0e939559ac4b50d73004
kjthorpe18 <kjt29@pitt.edu>
2023-02-25 15:47:35 -0500

    Add option for always full screen
    Adds argparse to replace the positional arguments scheme previously used. Two arguments exist for this controller, both defaulting to False:
    -e or --emulated, which replaces the e argument used before
    -f or --fullscreen, which tells the app to run in full screen always

--- impl/apps_v2/spotify_player.py
+++ impl/apps_v2/spotify_player.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageFont, ImageDraw
 from io import BytesIO
 
 class SpotifyScreen:
-    def __init__(self, config, modules):
+    def __init__(self, config, modules, fullscreen):
         self.modules = modules
 
         self.font = ImageFont.truetype("fonts/tiny.otf", 5)
@@ -13,6 +13,8 @@ class SpotifyScreen:
         self.title_color = (255,255,255)
         self.artist_color = (255,255,255)
         self.play_color = (102, 240, 110)
+
+        self.full_screen_always = fullscreen
 
         self.current_art_url = ''
         self.current_art_img = None
@@ -56,91 +58,104 @@ class SpotifyScreen:
         if response is not None:
             (artist, title, art_url, self.is_playing, progress_ms, duration_ms) = response
 
-            if not self.is_playing:
-                if not self.paused:
-                    self.paused_time = math.floor(time.time())
-                    self.paused = True
+            if self.full_screen_always:
+                if self.current_art_url != art_url:
+                    self.current_art_url = art_url
+                    response = requests.get(self.current_art_url)
+                    img = Image.open(BytesIO(response.content))
+                    self.current_art_img = img.resize((self.canvas_width, self.canvas_height), resample=Image.LANCZOS)
+
+                frame = Image.new("RGB", (self.canvas_width, self.canvas_height), (0,0,0))
+                draw = ImageDraw.Draw(frame)
+
+                frame.paste(self.current_art_img, (0,0))
+                return (frame, self.is_playing)
             else:
-                if self.paused and self.current_art_img and self.current_art_img.size == (self.canvas_width, self.canvas_height):
-                    self.title_animation_cnt = 0
-                    self.artist_animation_cnt = 0
-                    self.last_title_reset = math.floor(time.time())
-                    self.last_artist_reset = math.floor(time.time())
-                self.paused_time = math.floor(time.time())
-                self.paused = False
-
-            if (self.current_title != title or self.current_artist != artist):
-                self.current_artist = artist
-                self.current_title = title
-                self.title_animation_cnt = 0
-                self.artist_animation_cnt = 0
-                self.last_title_reset = math.floor(time.time())
-                self.last_artist_reset = math.floor(time.time())
-
-            current_time = math.floor(time.time())
-            show_fullscreen = current_time - self.paused_time >= self.paused_delay
-
-            # show fullscreen album art after pause delay
-            if show_fullscreen and self.current_art_img.size == (48, 48):
-                response = requests.get(self.current_art_url)
-                img = Image.open(BytesIO(response.content))
-                self.current_art_img = img.resize((self.canvas_width, self.canvas_height), resample=Image.LANCZOS)
-            elif not show_fullscreen and (self.current_art_url != art_url or self.current_art_img.size == (self.canvas_width, self.canvas_height)):
-                self.current_art_url = art_url
-                response = requests.get(self.current_art_url)
-                img = Image.open(BytesIO(response.content))
-                self.current_art_img = img.resize((48, 48), resample=Image.LANCZOS)
-
-            frame = Image.new("RGB", (self.canvas_width, self.canvas_height), (0,0,0))
-            draw = ImageDraw.Draw(frame)
-
-            # exit early if fullscreen
-            if self.current_art_img is not None:
-                if show_fullscreen:
-                    frame.paste(self.current_art_img, (0,0))
-                    return (frame, self.is_playing)
+                if not self.is_playing:
+                    if not self.paused:
+                        self.paused_time = math.floor(time.time())
+                        self.paused = True
                 else:
-                    frame.paste(self.current_art_img, (8,14))
+                    if self.paused and self.current_art_img and self.current_art_img.size == (self.canvas_width, self.canvas_height):
+                        self.title_animation_cnt = 0
+                        self.artist_animation_cnt = 0
+                        self.last_title_reset = math.floor(time.time())
+                        self.last_artist_reset = math.floor(time.time())
+                    self.paused_time = math.floor(time.time())
+                    self.paused = False
 
-            freeze_title = self.title_animation_cnt == 0 and self.artist_animation_cnt > 0
-            freeze_artist = self.artist_animation_cnt == 0 and self.title_animation_cnt > 0
-
-            title_len = self.font.getsize(self.current_title)[0]
-            artist_len = self.font.getsize(self.current_artist)[0]
-
-            text_length = self.canvas_width - 12
-            x_offset = 1
-            spacer = "     "
-
-            if title_len > text_length:
-                draw.text((x_offset-self.title_animation_cnt, 1), self.current_title + spacer + self.current_title, self.title_color, font = self.font)
-                if current_time - self.last_title_reset >= self.scroll_delay:
-                    self.title_animation_cnt += 1
-                if freeze_title or self.title_animation_cnt == self.font.getsize(self.current_title + spacer)[0]:
+                if (self.current_title != title or self.current_artist != artist):
+                    self.current_artist = artist
+                    self.current_title = title
                     self.title_animation_cnt = 0
-                    self.last_title_reset = math.floor(time.time())
-            else:
-                draw.text((x_offset-self.title_animation_cnt, 1), self.current_title, self.title_color, font = self.font)
-
-            if artist_len > text_length:
-                draw.text((x_offset-self.artist_animation_cnt, 7), self.current_artist + spacer + self.current_artist, self.artist_color, font = self.font)
-                if current_time - self.last_artist_reset >= self.scroll_delay:
-                    self.artist_animation_cnt += 1
-                if freeze_artist or self.artist_animation_cnt == self.font.getsize(self.current_artist + spacer)[0]:
                     self.artist_animation_cnt = 0
+                    self.last_title_reset = math.floor(time.time())
                     self.last_artist_reset = math.floor(time.time())
-            else:
-                draw.text((x_offset-self.artist_animation_cnt, 7), self.current_artist, self.artist_color, font = self.font)
 
-            draw.rectangle((0,0,0,12), fill=(0,0,0))
-            draw.rectangle((52,0,63,12), fill=(0,0,0))
+                current_time = math.floor(time.time())
+                show_fullscreen = current_time - self.paused_time >= self.paused_delay
 
-            line_y = 63
-            draw.rectangle((0,line_y-1,63,line_y), fill=(100,100,100))
-            draw.rectangle((0,line_y-1,0+round(((progress_ms / duration_ms) * 100) // 1.57), line_y), fill=self.play_color)
-            drawPlayPause(draw, self.is_playing, self.play_color)
-            
-            return (frame, self.is_playing)
+                # show fullscreen album art after pause delay
+                if show_fullscreen and self.current_art_img.size == (48, 48):
+                    response = requests.get(self.current_art_url)
+                    img = Image.open(BytesIO(response.content))
+                    self.current_art_img = img.resize((self.canvas_width, self.canvas_height), resample=Image.LANCZOS)
+                elif not show_fullscreen and (self.current_art_url != art_url or self.current_art_img.size == (self.canvas_width, self.canvas_height)):
+                    self.current_art_url = art_url
+                    response = requests.get(self.current_art_url)
+                    img = Image.open(BytesIO(response.content))
+                    self.current_art_img = img.resize((48, 48), resample=Image.LANCZOS)
+
+                frame = Image.new("RGB", (self.canvas_width, self.canvas_height), (0,0,0))
+                draw = ImageDraw.Draw(frame)
+
+                # exit early if fullscreen
+                if self.current_art_img is not None:
+                    if show_fullscreen:
+                        frame.paste(self.current_art_img, (0,0))
+                        return (frame, self.is_playing)
+                    else:
+                        frame.paste(self.current_art_img, (8,14))
+
+                freeze_title = self.title_animation_cnt == 0 and self.artist_animation_cnt > 0
+                freeze_artist = self.artist_animation_cnt == 0 and self.title_animation_cnt > 0
+
+                title_len = self.font.getsize(self.current_title)[0]
+                artist_len = self.font.getsize(self.current_artist)[0]
+
+                text_length = self.canvas_width - 12
+                x_offset = 1
+                spacer = "     "
+
+                if title_len > text_length:
+                    draw.text((x_offset-self.title_animation_cnt, 1), self.current_title + spacer + self.current_title, self.title_color, font = self.font)
+                    if current_time - self.last_title_reset >= self.scroll_delay:
+                        self.title_animation_cnt += 1
+                    if freeze_title or self.title_animation_cnt == self.font.getsize(self.current_title + spacer)[0]:
+                        self.title_animation_cnt = 0
+                        self.last_title_reset = math.floor(time.time())
+                else:
+                    draw.text((x_offset-self.title_animation_cnt, 1), self.current_title, self.title_color, font = self.font)
+
+                if artist_len > text_length:
+                    draw.text((x_offset-self.artist_animation_cnt, 7), self.current_artist + spacer + self.current_artist, self.artist_color, font = self.font)
+                    if current_time - self.last_artist_reset >= self.scroll_delay:
+                        self.artist_animation_cnt += 1
+                    if freeze_artist or self.artist_animation_cnt == self.font.getsize(self.current_artist + spacer)[0]:
+                        self.artist_animation_cnt = 0
+                        self.last_artist_reset = math.floor(time.time())
+                else:
+                    draw.text((x_offset-self.artist_animation_cnt, 7), self.current_artist, self.artist_color, font = self.font)
+
+                draw.rectangle((0,0,0,12), fill=(0,0,0))
+                draw.rectangle((52,0,63,12), fill=(0,0,0))
+
+                line_y = 63
+                draw.rectangle((0,line_y-1,63,line_y), fill=(100,100,100))
+                draw.rectangle((0,line_y-1,0+round(((progress_ms / duration_ms) * 100) // 1.57), line_y), fill=self.play_color)
+                drawPlayPause(draw, self.is_playing, self.play_color)
+                
+                return (frame, self.is_playing)
         else:
             #not active
             frame = Image.new("RGB", (self.canvas_width, self.canvas_height), (0,0,0))
---
+++ impl/controller_v4.py
@@ -0,0 +1,82 @@
+import os, inspect, sys, math, time, configparser, argparse
+from PIL import Image
+
+from apps_v2 import spotify_player
+from modules import spotify_module
+
+
+def main():
+    canvas_width = 64
+    canvas_height = 64
+
+    # get arguments
+    parser = argparse.ArgumentParser(
+                    prog = 'RpiSpotifyMatrixDisplay',
+                    description = 'Displays album art of currently playing song on an LED matrix')
+
+    parser.add_argument('-f', '--fullscreen', action='store_true')
+    parser.add_argument('-e', '--emulated', action='store_true')
+    args = parser.parse_args()
+
+    is_emulated = args.emulated
+    is_full_screen_always = args.fullscreen
+
+    # switch matrix library import if emulated
+    if is_emulated:
+        from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
+    else:
+        from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+    # get config
+    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    sys.path.append(currentdir+"/rpi-rgb-led-matrix/bindings/python")
+
+    config = configparser.ConfigParser()
+    parsed_configs = config.read('../config.ini')
+
+    if len(parsed_configs) == 0:
+        print("no config file found")
+        sys.exit()
+
+    # connect to Spotify and create display image
+    modules = { 'spotify' : spotify_module.SpotifyModule(config) }
+    app_list = [ spotify_player.SpotifyScreen(config, modules, is_full_screen_always) ]
+
+    # setup matrix
+    options = RGBMatrixOptions()
+    options.hardware_mapping = config.get('Matrix', 'hardware_mapping', fallback='regular')
+    options.rows = canvas_width
+    options.cols = canvas_height
+    options.brightness = 100 if is_emulated else config.getint('Matrix', 'brightness', fallback=100)
+    options.gpio_slowdown = config.getint('Matrix', 'gpio_slowdown', fallback=1)
+    options.limit_refresh_rate_hz = config.getint('Matrix', 'limit_refresh_rate_hz', fallback=0)
+    options.drop_privileges = False
+    matrix = RGBMatrix(options = options)
+
+    shutdown_delay = config.getint('Matrix', 'shutdown_delay', fallback=600)
+    black_screen = Image.new("RGB", (canvas_width, canvas_height), (0,0,0))
+    last_active_time = math.floor(time.time())
+
+    # generate image
+    while(True):
+        frame, is_playing = app_list[0].generate()
+        current_time = math.floor(time.time())
+
+        if frame is not None:
+            if is_playing:
+                last_active_time = math.floor(time.time())
+            elif current_time - last_active_time >= shutdown_delay:
+                frame = black_screen
+        else:
+            frame = black_screen
+
+        matrix.SetImage(frame)
+        time.sleep(0.08)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted with Ctrl-C')
+        sys.exit(0)
